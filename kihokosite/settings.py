cc30e784796fdc56b52c571b6a7a4193a8a1cad0
MJ <mikey.j.1228@gmail.com>
2023-04-30 20:49:05 +0900

    Refactor ALLOWED_HOSTS to be hard coded

--- kihokosite/settings.py
+++ kihokosite/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='127.0.0.1')]
+ALLOWED_HOSTS = ['kihoko.azurewebsites.net']
 
 
 # Application definition
