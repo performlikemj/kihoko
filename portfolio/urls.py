7d5d0c952ac84d929cb622c114cfd3ce1cbf1cd3
MJ <mikey.j.1228@gmail.com>
2023-09-26 09:59:52 +0900

    Add functionality to view merchandise image and description in detail

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('project/<slug:slug>/', views.project_detail, name='project_detail'),
     path('art_detail/<int:image_id>/', views.art_detail, name='art_detail'),
+    path('merchandise/<int:merch_id>/', views.merchandise_detail, name='merchandise_detail'),
     path('contact/', views.contact, name='contact'),
 ]
 
