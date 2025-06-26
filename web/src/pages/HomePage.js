b0b246042db65670410a8a4b73b002ab3641f8ae
MJ <54780948+performlikemj@users.noreply.github.com>
2025-06-26 23:15:33 +0900

    Create minimalist splash page and update routes

---
+++ web/src/pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const bgUrl = 'https://live.staticflickr.com/65535/54614150947_63ef8bf185_b.jpg';
+
+export default function HomePage() {
+  return (
+    <motion.div
+      className="home-page"
+      style={{ backgroundImage: `url(${bgUrl})` }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.6 }}
+    >
+      <div className="home-links">
+        <Link to="/art" className="btn btn-outline-light me-2">
+          Art
+        </Link>
+        <a href="mailto:kiho@kihoko.com" className="btn btn-outline-light">
+          Contact
+        </a>
+      </div>
+    </motion.div>
+  );
+}
