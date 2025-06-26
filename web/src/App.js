e2db3c9c5888b486ae6b5298570bc21686e453c4
MJ <mikey.j.1228@gmail.com>
2025-06-26 10:49:07 +0900

    Replace HomePage with CategoriesPage in routing and remove HomePage component

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/GlobalStyles';
 import { lightTheme, darkTheme } from './styles/themes';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
+import CategoriesPage from './pages/CategoriesPage';
 import ProjectDetailPage from './pages/ProjectDetailPage';
 import PortfolioPage from './pages/PortfolioPage';
 import ArtDetailPage from './pages/ArtDetailPage';
@@ -52,7 +52,7 @@ export default function App() {
         />
         <main className="page-transition loaded">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<CategoriesPage />} />
             <Route path="/category/:slug" element={<PortfolioPage />} />
             <Route path="/project/:slug" element={<ProjectDetailPage />} />
             <Route path="/art/:id" element={<ArtDetailPage />} />
